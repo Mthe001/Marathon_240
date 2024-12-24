2669b4c1840c8e89c66d986f0e9a453d7a9a1fc9
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-24 15:41:59 +0600

    fix:upcoming_marathon

--- src/pages/Home/MarathonCampaign.jsx
+++ src/pages/Home/MarathonCampaign.jsx
@@ -64,7 +64,7 @@ const MarathonCampaign = () => {
             <h2 className="text-2xl font-bold text-center mb-6 text-gray-800 dark:text-gray-100">
                 Upcoming  Marathon
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-6">
+            <div className="w-8/12 mx-auto grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
                 {marathons.map((marathon) => (
                     <div
                         key={marathon.id}
