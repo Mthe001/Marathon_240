2bb617663f02756a9d5fb81dae09bdaddddf6bbf
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-02-12 18:05:43 +0600

    fix(err):page-re-publish

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom'; // Import Link for navigation
 import Navbar from '../../shared/Navbar'; // Import Navbar component
 import Footer from '../../shared/Footer'; // Import Footer component
+import { BiError, BiErrorCircle } from 'react-icons/bi';
 
 const ErrorPage = () => {
     // Set document title using useEffect
@@ -12,18 +13,20 @@ const ErrorPage = () => {
     return (
         <div>
             <Navbar />
-            <div className="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-600 via-purple-500 to-pink-500 text-white">
+            <div className="flex items-center justify-center h-screen bg-gradient-to-r dark:bg-zinc-900 text-white">
                 <div className="text-center max-w-lg p-8 rounded-lg shadow-lg bg-zinc-800">
-                    <h1 className="text-6xl font-bold mb-4">Oops!</h1>
+                    <h1 className="text-6xl font-bold text-red-500 mb-4">Oops!</h1>
                     <p className="text-xl mb-6">
                         Something went wrong. We can't seem to find the page you're looking for.
                     </p>
+
                     <img
-                        src="https://img.icons8.com/ios/452/404.png" // Use a 404 error image
+                        src="https://i.ibb.co.com/whg3bZrP/Screenshot-2025-02-12-175817.png" 
                         alt="404"
-                        className="mx-auto mb-6"
-                        width="200"
+                        className="mx-auto rounded-xl mb-6"
+                        width="300"
                     />
+                    
                     <Link
                         to="/"
                         className="bg-blue-600 text-white py-2 px-6 rounded-md text-lg hover:bg-blue-700 transition-all duration-300"
